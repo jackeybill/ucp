292aa3e24263effad01deb7d70978ea482b5c47e
“qiaoqin.zhang@pwc.com” <qiaoqin.zhang@pwc.com>
2021-07-16 18:23:09 +0800

    view file source

--- src/pages/ProtocolSection/index.tsx
+++ src/pages/ProtocolSection/index.tsx
@@ -125,8 +125,9 @@ const ProtocolSection = (props: any) => {
     // setFilePath(`${baseUrl}${protocolTitleText}#page=${pageNum}`)
   },[pageNum])
   // const iframeStr = '<iframe src='+filePath+'></iframe>'; 
-
-  const iframeStr = `<iframe src=${require("../../assets/files/"+protocolTitleText)}#page=${pageNum}></iframe>`; 
+  // const iframeStr = `<iframe src=${require("../../assets/files/"+protocolTitleText)}#page=${pageNum}></iframe>`; 
+  
+  const iframeStr = `<iframe src=${process.env.PUBLIC_URL}/assets/${protocolTitleText}#page=${pageNum}></iframe>`; 
   const onIframe = ()=>{
     return  {__html: iframeStr}
   }
