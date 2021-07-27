3d3c7f01641a4f64a17951f40c06ee60d496da8e
“qiaoqin.zhang@pwc.com” <qiaoqin.zhang@pwc.com>
2021-07-27 14:03:29 +0800

    add endpoint

--- src/pages/ProtocolSection/index.tsx
+++ src/pages/ProtocolSection/index.tsx
@@ -35,7 +35,7 @@ export const sectionOptions = [
   { label: "EXCLUSION CRITERIA", value: "exclusionCriteria" },
   { label: "SCHEDULE OF ACTIVITIES", value: "scheduleActivities" },
   {
-    label: "OBJECTIVES, ENDPOINTS, ESTIMANDS",
+    label: "OBJECTIVES & ESTIMANDS",
     value: "objectivesEndpointsEstimands",
   },
 ];
