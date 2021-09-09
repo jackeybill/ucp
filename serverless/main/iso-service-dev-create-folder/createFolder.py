11c80b22bcdcc31dfb6a91b9f7da271fa424c1bc
Mandy Zhang <qiaoqin.zhang@pwc.com>
2021-09-09 16:04:36 +0800

    add create-folder lambda

---
+++ serverless/main/iso-service-dev-create-folder/createFolder.py
@@ -0,0 +1,11 @@
+import json
+import urllib
+from datetime import date
+import boto3
+
+bucket_name = 'ucp-filebucket-dev'
+
+def lambda_handler(event, context):
+    s3_client = boto3.client('s3')
+    directory_name="ucp-filebucket-dev/RawDocuments"
+    s3_client.put_object(Bucket=bucket_name, Key=(directory_name+'/'))  
