0e7e66a5ca2e6ed60d11c3572f5ecb1923643bc7
JoellaQLi <qiaoqiao.li@pwc.com>
2021-11-16 23:51:09 +0800

    Fix the bug of obj&end to show text if not rawtext

--- src/components/SectionsText/index.tsx
+++ src/components/SectionsText/index.tsx
@@ -146,7 +146,7 @@ const SectionText = (props: SectionTextIF) => {
                             props.file[key][s][0].raw && Object.values(props.file[key][s][0].raw.content).map((paragraph:EndpointParagraphIF,idx:number)=>{
                               // console.log( paragraph.text)
                               // const formattedText = paragraph.text&&paragraph.text.replace("\n",`<br/>&nbsp;&nbsp;&nbsp;&nbsp;<i class="my_symble">&#8226</i>&nbsp;&nbsp;`) || ""                          
-                              const formattedText = paragraph.rawtext&&paragraph.rawtext || ""                          
+                              const formattedText = paragraph.rawtext&&paragraph.rawtext ||paragraph.text&&paragraph.text|| ""                          
                               return(  
                                 <div className={paragraph.type==="heading"?"heading":"body"} key={idx}> 
                                   <div className="paragraph-name">{paragraph.name}</div>
