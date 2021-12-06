34a452aa8b421aafdcbd16ff5710462a4f4cd1e8
Darren Li <dong.a.li@pwc.com>
2021-12-07 02:14:34 +0800

    update sam template path

--- cfn-template/run.ps1
+++
@@ -1,3 +0,0 @@
-aws-vault exec main -- sam package --template-file .\codepipeline.yaml --output-template-file package.yaml --s3-bucket ucp-dev-for-testing-bucket5-1gtwhhhnxe8of
-
-aws-vault exec main -- sam deploy --template-file package.yaml --stack-name dhair2-demo-pipeline --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --region us-east-2
