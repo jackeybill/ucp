41a001c199a3b0dcd6c00ba3229144c4e06dbfef
JoellaQLi <qiaoqiao.li@pwc.com>
2022-01-14 15:52:34 +0800

    Mapping summary with rawcategory

--- src/components/TextWithEntity/index.tsx
+++ src/components/TextWithEntity/index.tsx
@@ -585,8 +585,10 @@ const TextWithEntity = (props: TextWithEntityIF) => {
           }
           if (word.type == "mark") {
             if (
+              // (activeType && word.category == activeType) ||
+              // activeType == "" || (activeType && word.rawcategory && (activeType ==="PROTECTED_HEALTH_INFORMATION" || activeType ==="TEST_TREATMENT_PROCEDURE" || activeType ==="TIME_EXPRESSION") && word.rawcategory == activeType) 
               (activeType && word.category == activeType) ||
-              activeType == "" || (activeType && word.rawcategory && (activeType ==="PROTECTED_HEALTH_INFORMATION" || activeType ==="TEST_TREATMENT_PROCEDURE" || activeType ==="TIME_EXPRESSION") && word.rawcategory == activeType) 
+              activeType == "" || (activeType && word.rawcategory && word.rawcategory == activeType) 
             ) {
               if (showTooltip) {
                 const id = word.id;
